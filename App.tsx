00e79ca104229a1a832bca75672f9505004e5f41
Robert Smith <rsmith5901@gmail.com>
2020-08-08 10:37:41 -0400

    adding typescript

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Text} from 'react-native';
+import {BleManager} from 'react-native-ble-plx';
 
-const App = () => {
+const App: React.FC = () => {
   return <Text>Hi adsfoiasjfdoijafsdoiafjsd</Text>;
 };
 

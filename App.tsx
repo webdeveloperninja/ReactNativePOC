313465397dbe7d5fd9ed7ec868cbf411bd70eb17
Robert Smith <rsmith5901@gmail.com>
2020-08-15 18:33:50 -0400

    adapter state as observable

--- App.tsx
+++ App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {BleManager} from 'react-native-ble-plx';
+import {Text, View} from 'react-native';
+import {BleManager, State} from 'react-native-ble-plx';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {useObservable} from 'rxjs-hooks';
+
+const selectAdapterState = (manager: BleManager): Observable<State> => {
+  const _adapterState$ = new BehaviorSubject<State>(State.Unknown);
+  const adapterState$ = _adapterState$.asObservable();
+  manager.onStateChange((newState) => _adapterState$.next(newState));
+
+  return adapterState$;
+};
 
 const App: React.FC = () => {
-  return <Text>Hi adsfoiasjfdoijafsdoiafjsd</Text>;
+  const bleManager = new BleManager();
+  const bluetoothAdapterState$ = selectAdapterState(bleManager);
+  const bluetoothAdapterState: State | null = useObservable(
+    () => bluetoothAdapterState$,
+  );
+
+  return (
+    <View>
+      <Text>BLE Application</Text>
+      <Text>Adapter State: {bluetoothAdapterState}</Text>
+    </View>
+  );
 };
 
 export default App;

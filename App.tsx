1cbaa841b66618b4025b51f08d01ea765630b0a2
Robert Smith <rsmith5901@gmail.com>
2020-08-16 00:05:00 -0400

    Getting device characteristics

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Text, View, ScrollView} from 'react-native';
+import {Text, Button, ScrollView} from 'react-native';
 import {BleManager, Device, State, LogLevel} from 'react-native-ble-plx';
 import {useObservable} from 'rxjs-hooks';
 import {selectAdapterState, selectDevices} from './ble';
+import {BehaviorSubject, from, EMPTY} from 'rxjs';
+import {tap, switchMap} from 'rxjs/operators';
 
 const App: React.FC = () => {
   const bleManager = new BleManager();
@@ -15,12 +17,48 @@ const App: React.FC = () => {
   );
   const devices: Device[] | null = useObservable(() => devices$);
 
+  const _selectedDevice$ = new BehaviorSubject<Device | null>(null);
+  const selectedDevice$ = _selectedDevice$.asObservable();
+
+  const onButtonPress = (device: Device) => _selectedDevice$.next(device);
+  const connectedDevice$ = selectedDevice$.pipe(
+    tap(() => bleManager.stopDeviceScan()),
+    switchMap((device) => {
+      if (!device) {
+        return EMPTY;
+      }
+      return from(device.connect());
+    }),
+  );
+  const discoveredServicesAndCharacteristics$ = connectedDevice$.pipe(
+    switchMap((device) => from(device.discoverAllServicesAndCharacteristics())),
+  );
+
+  const services$ = discoveredServicesAndCharacteristics$.pipe(
+    switchMap((device) => device.services()),
+  );
+
+  const characteristics$ = services$.pipe(
+    switchMap((services) =>
+      from(services).pipe(switchMap((service) => service.characteristics())),
+    ),
+  );
+
+  characteristics$.subscribe((characteristics) =>
+    console.log('characteristics', characteristics),
+  );
+
   return (
     <ScrollView>
       <Text>BLE Application</Text>
       <Text>Adapter State: {bluetoothAdapterState}</Text>
       {devices?.map((device, i) => (
-        <Text key={i}>{device.name}</Text>
+        <Button
+          key={i}
+          title={device.name ? device.name : ''}
+          onPress={() => onButtonPress(device)}
+          color="#f194ff"
+        />
       ))}
     </ScrollView>
   );

12a3030a19a9b79c6c1682a35a329eccddb2314f
Robert Smith <rsmith5901@gmail.com>
2020-08-19 19:57:24 -0400

    test

---
+++ __tests__/select-adapter.ts
@@ -0,0 +1,25 @@
+import {BleManager, State} from 'react-native-ble-plx';
+import {selectAdapterState} from '../ble';
+import {skip} from 'rxjs/operators';
+
+describe('selectAdapterState', () => {
+  it('should return initial state', (done) => {
+    const adapterState = State.PoweredOn;
+
+    const bleManager = {
+      onStateChange: (state) => {},
+      state: () => Promise.resolve(adapterState),
+    } as BleManager;
+
+    const adapterState$ = selectAdapterState(bleManager);
+
+    jest
+      .spyOn(bleManager, 'state')
+      .mockReturnValue(Promise.resolve(adapterState));
+
+    adapterState$.pipe(skip(1)).subscribe((state) => {
+      expect(state).toEqual(adapterState);
+      done();
+    });
+  });
+});

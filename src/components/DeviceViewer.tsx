2a565eee90c44e1813e96d602b4304d54e65225d
Robert Smith <rsmith5901@gmail.com>
2020-08-30 09:17:08 -0400

    using switch

--- src/components/DeviceViewer.tsx
+++ src/components/DeviceViewer.tsx
@@ -1,8 +1,8 @@
 import {decode} from 'base-64';
-import React, {useState} from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Switch, View} from 'react-native';
 import {Characteristic, Device} from 'react-native-ble-plx';
-import {Button, Card, RadioButton, Text, Title} from 'react-native-paper';
+import {Card, Text, Title} from 'react-native-paper';
 import {gpio2CharacteristicId, serviceId} from '../infrastructure/bluetooth';
 
 export type DeviceViewerProps = {
@@ -14,56 +14,41 @@ const DeviceViewer: React.FunctionComponent<DeviceViewerProps> = ({
   device,
   onSendDataToDevice,
 }) => {
-  const [value, setValue] = useState<string>();
-  const [characteristic, setCharacteristic] = useState<Characteristic>();
+  const [lastKnownPin2Value, setLastKnownPin2Value] = useState<
+    Characteristic
+  >();
+  const [assumedPin2Value, setAssumedPin2Value] = useState<boolean>();
 
-  const onChangeText = (text: string) => {
-    setValue(text);
-  };
-
-  const sendDataHandler = () => {
-    onSendDataToDevice(value ? value : '', device);
-  };
-
-  const getCharacteristics = async () => {
+  const getPin2Value = async () => {
     const characteristic = await device.readCharacteristicForService(
       serviceId,
       gpio2CharacteristicId,
     );
-    setCharacteristic(characteristic);
+    setLastKnownPin2Value(characteristic);
+
+    const value = !!Number(
+      decode(characteristic.value ? characteristic.value : ''),
+    );
+    setAssumedPin2Value(value);
   };
+
+  const toggleGpioPin2 = (value: boolean) => {
+    onSendDataToDevice(value ? 'start_process' : 'stop_process', device);
+    setAssumedPin2Value(value);
+  };
+
+  useEffect(() => {
+    getPin2Value();
+  }, []);
 
   return (
     <Card>
       <Title>{device.name}</Title>
-      <Text>{device.id} </Text>
 
       <View style={{flexDirection: 'row'}}>
-        <Text>Start Process</Text>
-        <RadioButton
-          value="start_process"
-          status={value === 'start_process' ? 'checked' : 'unchecked'}
-          onPress={() => onChangeText('start_process')}
-        />
+        <Text>GPIO Pin 2 </Text>
+        <Switch onValueChange={toggleGpioPin2} value={assumedPin2Value} />
       </View>
-      <View style={{flexDirection: 'row'}}>
-        <Text>End Process</Text>
-        <RadioButton
-          value="stop_process"
-          status={value === 'stop_process' ? 'checked' : 'unchecked'}
-          onPress={() => onChangeText('stop_process')}
-        />
-      </View>
-
-      <Button icon="broom" mode="contained" onPress={sendDataHandler}>
-        Send Command
-      </Button>
-
-      <Button icon="broom" mode="contained" onPress={getCharacteristics}>
-        Get Characteristics Value
-      </Button>
-
-      <Text>{!!characteristic ? decode(characteristic.value) : ''}</Text>
     </Card>
   );
 };
